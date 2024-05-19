5bd9209e1a4792800d7005ac62e60f7993492b6d
Blair Ednie <bednie@msudenver.edu>
2024-05-19 16:01:27 -0600

    Solve 189 using drain & extend

--- rotate_array_189/src/main.rs
+++ rotate_array_189/src/main.rs
@@ -1,3 +1,5 @@
+use std::{borrow::Borrow, mem};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -16,8 +18,18 @@ impl Solution {
     }
 }
 
-#[cfg(test)]
+struct Solution2;
 
+impl Solution2 {
+    pub fn rotate(mut nums: &mut Vec<i32>, k: i32) -> &Vec<i32> {
+        let i: usize = nums.len() - (k as usize % nums.len());
+        let x = nums.drain(0..i).as_slice().to_owned();
+        nums.extend_from_slice(&x);
+        nums
+    }
+}
+
+#[cfg(test)]
 mod test {
     use super::*;
 
@@ -25,6 +37,14 @@ mod test {
     fn test_rotate() {
         assert_eq!(
             Solution::rotate(&mut vec![1, 2, 3, 4, 5, 6, 7], 3),
+            &vec![5, 6, 7, 1, 2, 3, 4]
+        )
+    }
+
+    #[test]
+    fn test_rotate_2() {
+        assert_eq!(
+            Solution2::rotate(&mut vec![1, 2, 3, 4, 5, 6, 7], 3),
             &vec![5, 6, 7, 1, 2, 3, 4]
         )
     }
