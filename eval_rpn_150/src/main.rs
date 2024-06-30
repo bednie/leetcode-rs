0709f1257b8c2b026c5ede1bdedc01f2584a73f5
Blair Ednie <bednie@msudenver.edu>
2024-06-30 12:58:02 -0600

    Work on 150

---
+++ eval_rpn_150/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn eval_rpn(tokens: Vec<String>) -> i32 {
+        let mut stack = vec![];
+        for i in tokens.iter() {
+            match &i[..] {
+                "+" => { 
+                        let arg_1 = stack.pop().unwrap();
+                        let arg_2 = stack.pop().unwrap();
+                        stack.push(arg_2 + arg_1); 
+                    }
+                "*" => { 
+                        let arg_1 = stack.pop().unwrap();
+                        let arg_2 = stack.pop().unwrap();
+                        stack.push(arg_2 * arg_1); 
+                    }
+                "-" => { 
+                        let arg_1 = stack.pop().unwrap();
+                        let arg_2 = stack.pop().unwrap();
+                        stack.push(arg_2 - arg_1); 
+                    }
+                "/" => { 
+                        let arg_1 = stack.pop().unwrap();
+                        let arg_2 = stack.pop().unwrap();
+                        stack.push(arg_2 / arg_1);
+                    }
+                _ => { 
+                    if let Ok(token) = i.parse::<i32>() { 
+                        stack.push(token) 
+                    } else { 
+                        panic!("Something went wrong: unable to parse string to i32."); 
+                    }
+                }
+            }        
+        }
+        stack.pop().unwrap()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_eval_rpn() {
+        assert_eq!(Solution::eval_rpn(), );
+    }
+}
