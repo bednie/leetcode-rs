9981d3cd92466f3358294962256bcee221207577
Blair Ednie <bednie@msudenver.edu>
2024-06-30 18:38:48 -0600

    Solve 150

--- eval_rpn_150/src/main.rs
+++ eval_rpn_150/src/main.rs
@@ -9,34 +9,34 @@ impl Solution {
         let mut stack = vec![];
         for i in tokens.iter() {
             match &i[..] {
-                "+" => { 
-                        let arg_1 = stack.pop().unwrap();
-                        let arg_2 = stack.pop().unwrap();
-                        stack.push(arg_2 + arg_1); 
-                    }
-                "*" => { 
-                        let arg_1 = stack.pop().unwrap();
-                        let arg_2 = stack.pop().unwrap();
-                        stack.push(arg_2 * arg_1); 
-                    }
-                "-" => { 
-                        let arg_1 = stack.pop().unwrap();
-                        let arg_2 = stack.pop().unwrap();
-                        stack.push(arg_2 - arg_1); 
-                    }
-                "/" => { 
-                        let arg_1 = stack.pop().unwrap();
-                        let arg_2 = stack.pop().unwrap();
-                        stack.push(arg_2 / arg_1);
-                    }
-                _ => { 
-                    if let Ok(token) = i.parse::<i32>() { 
-                        stack.push(token) 
-                    } else { 
-                        panic!("Something went wrong: unable to parse string to i32."); 
+                "+" => {
+                    let arg_1 = stack.pop().unwrap();
+                    let arg_2 = stack.pop().unwrap();
+                    stack.push(arg_2 + arg_1);
+                }
+                "*" => {
+                    let arg_1 = stack.pop().unwrap();
+                    let arg_2 = stack.pop().unwrap();
+                    stack.push(arg_2 * arg_1);
+                }
+                "-" => {
+                    let arg_1 = stack.pop().unwrap();
+                    let arg_2 = stack.pop().unwrap();
+                    stack.push(arg_2 - arg_1);
+                }
+                "/" => {
+                    let arg_1 = stack.pop().unwrap();
+                    let arg_2 = stack.pop().unwrap();
+                    stack.push(arg_2 / arg_1);
+                }
+                _ => {
+                    if let Ok(token) = i.parse::<i32>() {
+                        stack.push(token)
+                    } else {
+                        panic!("Something went wrong: unable to parse string to i32.");
                     }
                 }
-            }        
+            }
         }
         stack.pop().unwrap()
     }
@@ -48,6 +48,23 @@ pub mod test {
 
     #[test]
     fn test_eval_rpn() {
-        assert_eq!(Solution::eval_rpn(), );
+        assert_eq!(
+            Solution::eval_rpn(vec![
+                "10".to_string(),
+                "6".into(),
+                "9".into(),
+                "3".into(),
+                "+".into(),
+                "-11".into(),
+                "*".into(),
+                "/".into(),
+                "*".into(),
+                "17".into(),
+                "+".into(),
+                "5".into(),
+                "+".into()
+            ]),
+            22
+        );
     }
 }
