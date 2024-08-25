1e6ac7b85d64b029bf148d57d2640ed4d6b483d3
Blair Ednie <bednie@msudenver.edu>
2024-08-25 14:04:03 -0600

    Redo with String instead of slice

--- generate_parentheses_22/src/main.rs
+++ generate_parentheses_22/src/main.rs
@@ -1,31 +1,41 @@
 use core::str;
+use std::time::{Duration, Instant};
 
 fn main() { 
-    dbg!(Solution::generate_parenthesis(3));
+    let start: Instant = Instant::now();
+    let n = 5; 
+    Solution::generate_parenthesis(n);
+    let duration: Duration = start.elapsed();
+    println!("Time elapsed to generate {} pairs of () is: {:?}", n, duration);
 }
 
 struct Solution;
 
 impl Solution {
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
-        fn backtrack(pairs: &str, opened: u8, closed: u8, result: &mut Vec<String>, n: u8) {
-            if pairs.len() as u8 == n * 2 {
-                result.push(String::from(pairs));
+        fn backtrack(pairs: &mut String, opened: u8, closed: u8, result: &mut Vec<String>, n: u8) {
+            if opened + closed == pairs.capacity() as u8 {
+                result.push(pairs.clone());
                 return;
             }
 
             if opened < n {
-                backtrack(&[pairs, "("].concat(), opened + 1, closed, result, n);
+                pairs.push('(');
+                backtrack(pairs, opened + 1, closed, result, n);
+                pairs.pop();
             }
 
             if closed < opened {
-                backtrack(&[pairs, ")"].concat(), opened, closed + 1, result, n);
+                pairs.push(')');
+                backtrack(pairs, opened, closed + 1, result, n);
+                pairs.pop();
             }
         }
 
         let mut result: Vec<String> = vec![];
-        let pair: &str = "(";
-        backtrack(pair, 1, 0, &mut result, n as u8);
+        let mut pairs = String::with_capacity((n*2) as usize);
+        pairs.push('(');
+        backtrack(&mut pairs, 1, 0, &mut result, n as u8);
         result
     }
 }
