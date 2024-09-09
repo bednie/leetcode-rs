fcfde5b288344c1adac15a76f75b876dbfc0cf97
Blair Ednie <bednie@msudenver.edu>
2024-09-08 20:30:14 -0600

    Use as_bytes

--- excel_sheet_171/src/main.rs
+++ excel_sheet_171/src/main.rs
@@ -7,8 +7,9 @@ struct Solution;
 impl Solution {
     pub fn title_to_number(column_title: String) -> i32 {
         column_title
-            .chars()
-            .fold(0, |acc: i32, c: char| acc * 26 + (c as u8 - b'@') as i32)
+            .as_bytes()
+            .iter()
+            .fold(0, |acc: i32, c: &u8| acc * 26 + (c - b'@') as i32)
     }
 }
 
