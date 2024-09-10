1f32ede1428acd01797c59d64680f9c7d1d21a00
Blair Ednie <bednie@msudenver.edu>
2024-09-09 21:25:42 -0600

    Redo with map & fold

--- xor_queries_1310/src/main.rs
+++ xor_queries_1310/src/main.rs
@@ -1,21 +1,21 @@
 fn main() {
-    dbg!(Solution::xor_queries(vec![1,3,4,8], vec![vec![0,1],vec![1,2],vec![0,3],vec![3,3]]));
+    dbg!(Solution::xor_queries(
+        vec![1, 3, 4, 8],
+        vec![vec![0, 1], vec![1, 2], vec![0, 3], vec![3, 3]]
+    ));
 }
 
 struct Solution;
 
 impl Solution {
     pub fn xor_queries(arr: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut result = vec![];
-
-        queries.iter().for_each(|q| {
-            let mut x = 0;
-            (q[0] as usize..=q[1] as usize).for_each(|i| {
-                x ^= arr[i];
-            });
-            result.push(x);
-        });
-        result
+        queries
+            .iter()
+            .map(|q| -> i32 {
+                (q[0] as usize..=q[1] as usize)
+                .fold(0, |acc: i32, c: usize| acc ^ arr[c])
+            })
+            .collect()
     }
 }
 
@@ -26,8 +26,11 @@ pub mod test {
     #[test]
     fn test_xor_queries() {
         assert_eq!(
-            Solution::xor_queries(vec![1,3,4,8], vec![vec![0,1],vec![1,2],vec![0,3],vec![3,3]]),
-            vec![2,7,14,8]
+            Solution::xor_queries(
+                vec![1, 3, 4, 8],
+                vec![vec![0, 1], vec![1, 2], vec![0, 3], vec![3, 3]]
+            ),
+            vec![2, 7, 14, 8]
         );
     }
 }
