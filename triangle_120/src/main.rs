7ccd109b644cc488099a47063564d9aae9d0c36a
Blair Ednie <bednie@msudenver.edu>
2024-09-13 17:26:35 -0600

    Solve 120

---
+++ triangle_120/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    dbg!(Solution::minimum_total(vec![
+        vec![2],
+        vec![3, 4],
+        vec![6, 5, 7],
+        vec![4, 1, 8, 3]
+    ]));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn minimum_total(triangle: Vec<Vec<i32>>) -> i32 {
+        let mut triangle = triangle;
+
+        for row in 1..triangle.len() {
+            for i in 0..triangle[row].len() {
+                if i == 0 {
+                    triangle[row][i] += triangle[row - 1][0];
+                } else {
+                    triangle[row][i] += triangle[row - 1][(i - 1).min(triangle[row - 1].len() - 1)]
+                        .min(triangle[row - 1][i.min(triangle[row - 1].len() - 1)]);
+                }
+            }
+        }
+        *triangle[triangle.len() - 1].iter().min().unwrap()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_minimum_total() {
+        assert_eq!(
+            Solution::minimum_total(vec![vec![2], vec![3, 4], vec![6, 5, 7], vec![4, 1, 8, 3]]),
+            11
+        );
+    }
+}
