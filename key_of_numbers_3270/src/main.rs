57a63f1c3b90a05c8eff8d905637f732fcf1fbc1
Blair Ednie <bednie@msudenver.edu>
2024-09-17 18:19:59 -0600

    Clean up

--- key_of_numbers_3270/src/main.rs
+++ key_of_numbers_3270/src/main.rs
@@ -6,24 +6,19 @@ struct Solution;
 
 impl Solution {
     pub fn generate_key(num1: i32, num2: i32, num3: i32) -> i32 {
-        let mut key = std::cmp::min(num1 % 10, std::cmp::min(num2 % 10, num3 % 10));
-
-        key += std::cmp::min(
-            num1 / 10 % 10,
-            std::cmp::min(num2 / 10 % 10, num3 / 10 % 10),
-        ) * 10;
-
-        key += std::cmp::min(
-            num1 / 100 % 10,
-            std::cmp::min(num2 / 100 % 10, num3 / 100 % 10),
-        ) * 100;
-
-        key += std::cmp::min(
-            num1 / 1000 % 10,
-            std::cmp::min(num2 / 1000 % 10, num3 / 1000 % 10),
-        ) * 1000;
-
-        key
+        std::cmp::min(num1 % 10, std::cmp::min(num2 % 10, num3 % 10))
+            + std::cmp::min(
+                num1 / 10 % 10,
+                std::cmp::min(num2 / 10 % 10, num3 / 10 % 10),
+            ) * 10
+            + std::cmp::min(
+                num1 / 100 % 10,
+                std::cmp::min(num2 / 100 % 10, num3 / 100 % 10),
+            ) * 100
+            + std::cmp::min(
+                num1 / 1000 % 10,
+                std::cmp::min(num2 / 1000 % 10, num3 / 1000 % 10),
+            ) * 1000
     }
 }
 
