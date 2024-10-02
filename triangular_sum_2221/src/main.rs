94e146c6ff47836ee252faba30609bb953e83c25
Blair Ednie <bednie@msudenver.edu>
2024-10-02 16:57:32 -0600

    Solve 2221

---
+++ triangular_sum_2221/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    dbg!(Solution::triangular_sum(vec![1, 2, 3, 4, 5]));
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn triangular_sum(nums: Vec<i32>) -> i32 {
+        if nums.len() == 1 {
+            return nums[0];
+        }
+
+        Self::triangular_sum(
+            nums.windows(2)
+                .map(|w: &[i32]| w.iter().sum::<i32>() % 10)
+                .collect(),
+        )
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_triangular_sum() {
+        assert_eq!(Solution::triangular_sum(vec![1, 2, 3, 4, 5]), 8);
+    }
+}
