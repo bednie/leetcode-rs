ce395cb79ed628f14a547b3c11465cb5bf69e05f
Blair Ednie <bednie@msudenver.edu>
2024-10-02 17:08:07 -0600

    Use if-else

--- triangular_sum_2221/src/main.rs
+++ triangular_sum_2221/src/main.rs
@@ -7,14 +7,14 @@ struct Solution;
 impl Solution {
     pub fn triangular_sum(nums: Vec<i32>) -> i32 {
         if nums.len() == 1 {
-            return nums[0];
+            nums[0]
+        } else {
+            Self::triangular_sum(
+                nums.windows(2)
+                    .map(|w: &[i32]| w.iter().sum::<i32>() % 10)
+                    .collect(),
+            )
         }
-
-        Self::triangular_sum(
-            nums.windows(2)
-                .map(|w: &[i32]| w.iter().sum::<i32>() % 10)
-                .collect(),
-        )
     }
 }
 
