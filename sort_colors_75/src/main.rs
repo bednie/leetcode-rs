ed4ae4cb7d7aa02618668f765ac4157ff1fd84e4
Blair Ednie <bednie@m4.local>
2024-12-13 15:58:44 -0700

    Solve 75

---
+++ sort_colors_75/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    dbg!()
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn sort_colors(nums: &mut Vec<i32>) {
+        nums.sort_unstable()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_sort_colors() {
+        let mut v = vec![2, 0, 2, 1, 1, 0];
+        Solution::sort_colors(&mut v);
+        assert_eq!(v, vec![0, 0, 1, 1, 2, 2]);
+    }
+}
