8e5542058c2ca5eec56a77cd4840c178a385f444
Blair Ednie <bednie.msudenver@gmail.com>
2024-12-28 18:59:35 -0700

    fmt

--- max_product_628/src/main.rs
+++ max_product_628/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     dbg!(
-        Solution::maximum_product(vec![-1,-2,-3]),
-        Solution::maximum_product(vec![2,1,3,4]),
-        Solution::maximum_product(vec![3,-1,-2,1,2])
+        Solution::maximum_product(vec![-1, -2, -3]),
+        Solution::maximum_product(vec![2, 1, 3, 4]),
+        Solution::maximum_product(vec![3, -1, -2, 1, 2])
     );
 }
 
@@ -12,7 +12,12 @@ impl Solution {
     pub fn maximum_product(nums: Vec<i32>) -> i32 {
         let mut nums = nums;
         nums.sort_unstable();
-        nums[0..3].iter().product::<i32>().max(nums[0..2].iter().product::<i32>() * nums[nums.len()-1]).max(nums[0] * nums[nums.len()-2..].iter().product::<i32>()).max(nums[(nums.len() as i32 - 3).max(0) as usize..].iter().product::<i32>())
+        nums[0..3]
+            .iter()
+            .product::<i32>()
+            .max(nums[0..2].iter().product::<i32>() * nums[nums.len() - 1])
+            .max(nums[0] * nums[nums.len() - 2..].iter().product::<i32>())
+            .max(nums[nums.len() - 3..].iter().product::<i32>())
     }
 }
 
@@ -22,8 +27,8 @@ pub mod test {
 
     #[test]
     fn test_maximum_product() {
-        assert_eq!(Solution::maximum_product(vec![-1,-2,-3]), -6);
-        assert_eq!(Solution::maximum_product(vec![2,1,3,4]), 24);
-        assert_eq!(Solution::maximum_product(vec![3,-1,-2,1,2]), 6);
+        assert_eq!(Solution::maximum_product(vec![-1, -2, -3]), -6);
+        assert_eq!(Solution::maximum_product(vec![2, 1, 3, 4]), 24);
+        assert_eq!(Solution::maximum_product(vec![3, -1, -2, 1, 2]), 6);
     }
 }
